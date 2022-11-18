fa892144bc24398239ea30113e27d0f1289f70b8
geno <gabriele.genovese2@studio.unibo.it>
2022-11-18 15:07:00 +0100

    fix errors

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE events (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   show int NOT NULL,
   title text NOT NULL,
   location int NOT NULL,
@@ -7,12 +7,12 @@ CREATE TABLE events (
   end datetime NOT NULL,
 
   PRIMARY KEY (id),
-  FOREIGN KEY (show) REFERENCES (shows),
-  FOREIGN KEY (location) REFERENCES (locations)
-)
+  FOREIGN KEY (show) REFERENCES shows,
+  FOREIGN KEY (location) REFERENCES locations
+);
 
 CREATE TABLE shows (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   title text NOT NULL,
   artist int NOT NULL,
   siae_price float NOT NULL,
@@ -20,18 +20,18 @@ CREATE TABLE shows (
 
   CHECK(cachet > 0 AND siae_price > 0),
   PRIMARY KEY (id),
-  FOREIGN KEY (artist) REFERENCES (artists)
-)
+  FOREIGN KEY (artist) REFERENCES artists
+);
 
 CREATE TABLE artists (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   name text NOT NULL,
   
   PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE locations (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   type int NOT NULL,
   name text NOT NULL,
   coords point NOT NULL,
@@ -39,19 +39,19 @@ CREATE TABLE locations (
 
   CHECK(price > 0),
   PRIMARY KEY (id),
-  FOREIGN KEY (type) REFERENCES (location_types)
-)
+  FOREIGN KEY (type) REFERENCES location_types
+);
 
 CREATE TABLE location_sectors (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   location int NOT NULL,
   name text NOT NULL,
   capacity int NOT NULL,
 
   CHECK(capacity > 0),
   PRIMARY KEY (id),
-  FOREIGN KEY (location) REFERENCES (locations)
-)
+  FOREIGN KEY (location) REFERENCES locations
+);
 
 CREATE TABLE location_sector_events (
   sector int NOT NULL,
@@ -60,50 +60,50 @@ CREATE TABLE location_sector_events (
 
   CHECK(price > 0),
   UNIQUE (sector, event),
-  FOREIGN KEY (sector) REFERENCES (sectors),
-  FOREIGN KEY (event) REFERENCES (events)
-)
+  FOREIGN KEY (sector) REFERENCES sectors,
+  FOREIGN KEY (event) REFERENCES events
+);
 
 CREATE TABLE location_types (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   name varchar(256) NOT NULL,
 
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE service_providers (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   type int NOT NULL,
   price float NOT NULL,                                    -- per hour
 
   CHECK(price > 0),
   PRIMARY KEY (id),
-  FOREIGN KEY (type) REFERENCES (service_types)
-)
+  FOREIGN KEY (type) REFERENCES service_types
+);
 
 CREATE TABLE service_types (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   name varchar(256) NOT NULL,
   description text NOT NULL,
 
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE service_providers_events (
   provider int NOT NULL, 
   event int NOT NULL,
 
-  FOREING KEY (provider) REFENCES (service_providers),
-  FOREING KEY (event) REFENCES (events),
+  FOREIGN KEY (provider) REFERENCES service_providers,
+  FOREIGN KEY (event) REFERENCES events,
   UNIQUE (provider, event)
-)
+);
 
 CREATE TABLE tickets (
-  id int NOT NULL AUTO_INCREMENT,
+  id serial,
   sector int NOT NULL,
   event int NOT NULL,
 
   PRIMARY KEY (id),
-  FOREIGN KEY (sector) REFERENCES (location_sectors),
-  FOREIGN KEY (event) REFERENCES (events)
-)
+  FOREIGN KEY (sector) REFERENCES location_sectors,
+  FOREIGN KEY (event) REFERENCES events
+);
