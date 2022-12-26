4874fdbfe40b142b8edd5d6a879c4c352b4d3b13
giammirove <rovelli.gianmaria@gmail.com>
2022-12-26 20:35:19 +0100

    Added show-max-earnings, to be checked

---
+++ queries/search-earnings-by-show.sql
@@ -0,0 +1,12 @@
+SELECT SUM (
+  SELECT SUM(sep.price) - (
+    SELECT s.siae_price + s.cachet + SUM(spe.price) + v.price
+    FROM shows AS s, events AS e, events_service_providers_serve AS spe, venues AS v
+    WHERE e.show = s.id AND spe.event = e.id AND e.venue = v.id AND e.id =  t_e.id
+  ) expenses
+  FROM seats AS s, sectors_events_cost AS sec, tickets AS t
+  WHERE s.sector = sep.sector AND t.seat = s.id
+    AND sep.event = t_e.id AND t.event = t_e.id
+  ) 
+FROM events AS t_e
+WHERE t_e.show = ... 
--- queries/show-max-earnings.sql
+++ queries/show-max-earnings.sql
@@ -1,16 +1,19 @@
-SELECT (
-  SELECT SUM(sep.price) - (
-    SELECT s.siae_price + s.cachet + SUM(spe.price) + v.price
-    FROM shows AS s, events AS e, events_service_providers_serve AS spe, venues AS v
-    WHERE e.show = s.id AND spe.event = e.id AND e.venue = v.id AND e.id = ...
-  ) expenses
-  FROM seats AS s, sectors_events_cost AS sec, tickets AS t, events AS e
-  WHERE s.sector = sep.sector
-    AND t.seat = s.id
-    AND sep.event = e.id
-    AND t.event = e.id
-    AND e.starts_by = ...
-    AND e.ends_by = ...
-  ) earnings
-  ORDER BY earnings DESC
-  LIMIT 1
+SELECT t_sh.title, (
+  SELECT SUM (
+    SELECT SUM(sep.price) - (
+      SELECT s.siae_price + s.cachet + SUM(spe.price) + v.price
+      FROM shows AS s, events AS e, events_service_providers_serve AS spe, venues AS v
+      WHERE e.show = s.id AND spe.event = e.id AND e.venue = v.id AND e.id =  t_e.id
+    ) expenses
+    FROM seats AS s, sectors_events_cost AS sec, tickets AS t
+    WHERE s.sector = sep.sector AND t.seat = s.id
+      AND sep.event = t_e.id AND t.event = t_e.id
+  ) earnings 
+  FROM events AS t_e 
+  WHERE t_e.show = t_sh.id
+    AND t_e.starts_at >= ...
+    AND t_e.ends_at <= ...
+) show_earnings
+FROM show as t_sh
+ORDER BY show_earnings DESC
+LIMIT 1
