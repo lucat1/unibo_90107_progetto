68a8295d659a8907582f6dc74830d1004ab0e8e2
geno <gabriele.genovese2@studio.unibo.it>
2023-01-02 12:23:14 +0100

    update relazione.tex

--- views.sql
+++ views.sql
@@ -1,6 +1,7 @@
-CREATE VIEW spesa_evento AS 
+CREATE VIEW spesa_evento AS
 SELECT
-  e.id as evento, s.prezzo_siae + s.cachet + l.prezzo + SUM(efs.prezzo) AS "spesa"
+  e.id as evento,
+  s.prezzo_siae + s.cachet + l.prezzo + SUM(efs.prezzo) AS "spesa"
 FROM
   spettacolo AS s,
   evento AS e,
@@ -10,11 +11,16 @@ WHERE
   e.spettacolo = s.id
   AND efs.evento = e.id
   AND e.luogo = l.id
-GROUP BY e.id, s.prezzo_siae,s.cachet,l.prezzo;
+GROUP BY
+  e.id,
+  s.prezzo_siae,
+  s.cachet,
+  l.prezzo;
 
 CREATE VIEW incasso_evento AS
 SELECT
-  b.evento, SUM(sec.prezzo) as "incasso"
+  b.evento,
+  SUM(sec.prezzo) as "incasso"
 FROM
   posto AS p,
   settore_evento_costo AS sec,
@@ -23,19 +29,24 @@ WHERE
   p.settore = sec.settore
   AND b.posto = p.id
   AND sec.evento = b.evento
-GROUP BY b.evento;
+GROUP BY
+  b.evento;
 
 CREATE VIEW guadagno_evento AS
 SELECT
-  ie.evento evento, (ie.incasso - se.spesa) guadagno
+  ie.evento evento,
+  (ie.incasso - se.spesa) guadagno
 FROM
-  spesa_evento AS se
-NATURAL JOIN incasso_evento as ie
-GROUP BY ie.evento, guadagno;
+  spesa_evento AS se NATURAL
+  JOIN incasso_evento as ie
+GROUP BY
+  ie.evento,
+  guadagno;
 
-CREATE VIEW guadagno_spettacolo AS 
+CREATE VIEW guadagno_spettacolo AS
 SELECT
-  s.id spettacolo, SUM(ge.guadagno) guadagno
+  s.id spettacolo,
+  SUM(ge.guadagno) guadagno
 FROM
   spettacolo AS s,
   evento AS e,
@@ -43,16 +54,28 @@ FROM
 WHERE
   e.id = ge.evento
   AND e.spettacolo = s.id
-GROUP BY s.id;
+GROUP BY
+  s.id;
 
 CREATE VIEW percentuale_biglietti_evento AS
 SELECT
-  be.evento AS evento, be.biglietti * 100.0 / SUM(s.capienza) AS percentuale
+  be.evento AS evento,
+  be.biglietti * 100.0 / SUM(s.capienza) AS percentuale
 FROM
   settore_evento_costo AS sec,
-  (SELECT b.evento AS evento, COUNT(*) AS biglietti FROM biglietto AS b GROUP BY b.evento) AS be,
+  (
+    SELECT
+      b.evento AS evento,
+      COUNT(*) AS biglietti
+    FROM
+      biglietto AS b
+    GROUP BY
+      b.evento
+  ) AS be,
   settore AS s
 WHERE
   s.id = sec.settore
   AND be.evento = sec.evento
-GROUP BY be.evento,be.biglietti;
+GROUP BY
+  be.evento,
+  be.biglietti;
